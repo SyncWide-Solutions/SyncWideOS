be6b849dfc29bb2848dd27c9d3aedd81043c1b15
LolgamerHD <dahlemlinus@gmail.com>
2025-02-11 14:30:31 +0100

    Update FILES

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -44,9 +44,45 @@ clear_screen:
     int 0x10
     ret
 
+; Sound routine - Add this after your existing procedures
+play_sound:
+    ; save registers
+    push ax
+    push bx
+    push cx
+    
+    mov al, 182         ; Prepare the speaker for the note
+    out 43h, al         
+    mov ax, 2280        ; Frequency number (higher = lower sound)
+    out 42h, al         ; Output low byte
+    mov al, ah          ; Output high byte
+    out 42h, al 
+    in al, 61h         ; Turn on note (get value from port 61h)
+    or al, 00000011b   ; Set bits 1 and 0
+    out 61h, al        ; Send new value
+    
+    ; Add delay for sound duration
+    mov cx, 0xFFFF
+    
+.delay:
+    loop .delay
+    
+    ; Turn off speaker
+    in al, 61h         ; Turn off note (get value from port 61h)
+    and al, 11111100b  ; Reset bits 1 and 0
+    out 61h, al        ; Send new value
+    
+    ; restore registers
+    pop cx
+    pop bx
+    pop ax
+    ret
+
 main:
 
     call clear_screen
+
+    call play_sound
 
     ; setup data segments
     mov ax, 0           ; can't set ds/es directly
