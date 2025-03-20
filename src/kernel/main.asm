cd2a1d169c0445203182a14356af2be4ed3d7d0c
LolgamerHD <dahlemlinus@gmail.com>
2025-03-20 12:31:17 +0100

    Update Kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,4 +1,3 @@
-; Just an old backup file of the kernel
 org 0x7C00
 bits 16
 
@@ -38,39 +37,8 @@ puts:
     pop si    
     ret
     
-; Clear screen function
-clear_screen:
-    push ax
-    push bx
-    push cx
-    push dx
-
-    mov ah, 0x00    ; Set video mode function
-    mov al, 0x03    ; Text mode 80x25 16 colors
-    int 0x10        ; BIOS video interrupt
-
-    mov ah, 0x06    ; Scroll up function
-    mov al, 0x00    ; Clear entire screen
-    mov bh, 0x07    ; White on black attribute
-    mov cx, 0x0000  ; Upper left corner (0,0)
-    mov dx, 0x184F  ; Lower right corner (79,24)
-    int 0x10        ; BIOS video interrupt
-
-    mov ah, 0x02    ; Set cursor position
-    mov bh, 0x00    ; Page 0
-    mov dx, 0x0000  ; Row 0, Column 0
-    int 0x10        ; BIOS video interrupt
-
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    ret
 
 main:
-
-    call clear_screen
-
     ; setup data segments
     mov ax, 0           ; can't set ds/es directly
     mov ds, ax
